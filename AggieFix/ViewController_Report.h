43849c42c4e1c7abb0c287cd0012b692d41ed622
harumhl <haru.mhl@tamu.edu>
2015-10-08 00:12:51 -0500

    8. photo and comment color. comments

--- AggieFix/ViewController_Report.h
+++ AggieFix/ViewController_Report.h
@@ -7,12 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h> // for sending email
 
 @interface ViewController_Report : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate> {
     
-    IBOutlet UIScrollView *scroller;
-    IBOutlet UITextField *titleText;
-    IBOutlet UILabel *loaded;
+    IBOutlet UIScrollView *scroller;    // Making the screen scrollable
+    IBOutlet UITextField *titleText;    // title text field
+    IBOutlet UILabel *loaded;           // TEMPORARY label to check "save" and "load" works
         
     UIImagePickerController* takePhoto;
     UIImagePickerController* loadPhoto;
@@ -20,15 +21,19 @@
     IBOutlet UIImageView *imageView;
 }
 
+// Save data from text field
 // https://www.youtube.com/watch?v=NavVADVU6fk
-- (IBAction)saveText:(id)sender;
-- (IBAction)loadText:(id)sender;
-- (IBAction)dismissKeyboard:(id)sender;
+- (IBAction)saveText:(id)sender;        // TEMPORARY function to show text saving from title field
+- (IBAction)loadText:(id)sender;        // TEMPORARY function to show text loading from saved
+- (IBAction)dismissKeyboard:(id)sender; // dismiss keyboard when hit "return" FOR ONLY TITLE
 
-// https://www.youtube.com/watch?v=4s-C3f6kQp4
+// Shows two (or three, third to cancel) options to choose photos from
+// https://www.youtube.com/watch?v=4s-C3f6kQp4 display alert button list
+// https://www.youtube.com/watch?v=T7COfFjhXo8 button -> camera, photo app
 - (IBAction)photoOption:(id)sender;
 
-// https://www.youtube.com/watch?v=T7COfFjhXo8
-- (IBAction)takePhoto:(id)sender;
-- (IBAction)chooseExisting:(id)sender;
+// Send email NOT FULLY IMPLEMENTED YET B/C OF SIMULATOR LIMITATION
+// https://www.youtube.com/watch?v=00rKDuIqEt0
+// https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/SystemMessaging_TopicsForIOS/Articles/SendingaMailMessage.html
+- (IBAction)displayComposerSheet:(id)sender;
 @end
--- AggieFix/ViewController_Report.m
+++ AggieFix/ViewController_Report.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    // Making the screen scrollable
     [scroller setScrollEnabled:YES];
     [scroller setContentSize:CGSizeMake(320, 768)];
 }
@@ -36,12 +37,14 @@
     // Pass the selected object to the new view controller.
 }
 */
+
 // Exiting back to the main menu
 - (IBAction)exit:(id)sender {
     
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
-
+//==============================================================================
+// TEMPORARY function to show text saving from title field && loading from saved
 - (IBAction)saveText:(id)sender {
     NSString *saveTitleText = titleText.text;
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
@@ -58,40 +61,43 @@
 - (IBAction)dismissKeyboard:(id)sender {
     [sender resignFirstResponder];
 }
-
+//==============================================================================
 - (IBAction)photoOption:(id)sender {
     UIAlertController *photoOption = [UIAlertController alertControllerWithTitle:@"Photo option" message:@"" preferredStyle:UIAlertViewStyleDefault];
     
     // 1st option
     UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"Take a picture" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        // Action to be taken -> bring camera
         takePhoto = [[UIImagePickerController alloc] init];
         takePhoto.delegate = self;
         [takePhoto setSourceType:UIImagePickerControllerSourceTypeCamera];
         [self presentViewController:takePhoto animated:YES completion:NULL];
     }];
-    [photoOption addAction:defaultAction];
+    [photoOption addAction:defaultAction]; // put the alert in the alertController list
     
     // 2nd option
     UIAlertAction* anotherAction = [UIAlertAction actionWithTitle:@"Get photo from photo app" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
-        takePhoto = [[UIImagePickerController alloc] init];
-        takePhoto.delegate = self;
-        [takePhoto setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
-        [self presentViewController:takePhoto animated:YES completion:NULL];
+        // Action to be taken -> bring photo app
+        loadPhoto = [[UIImagePickerController alloc] init];
+        loadPhoto.delegate = self;
+        [loadPhoto setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+        [self presentViewController:loadPhoto animated:YES completion:NULL];
     }];
-    [photoOption addAction:anotherAction];
+    [photoOption addAction:anotherAction]; // put the alert in the alertController list
     
-    // Cancel?!
-    UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+    // Cancel option
+    UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) { // do nothing but just dismiss the option list
         [self dismissViewControllerAnimated:YES completion:NULL];
     }];
-    [photoOption addAction:cancelAction];
+    [photoOption addAction:cancelAction]; // put the alert in the alertController list
 
 
     // present
     [self presentViewController:photoOption animated:YES completion:nil];
 }
 
-// https://www.youtube.com/watch?v=T7COfFjhXo8
+//==============================================================================
+// Get the image from either camera or photo app and display it
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
     image = [info objectForKey:UIImagePickerControllerOriginalImage];
     [imageView setImage:image];
@@ -99,6 +105,56 @@
 }
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
     [self dismissViewControllerAnimated:YES completion:NULL];
+}
+//==============================================================================
+
+
+
+
+
+// Send email
+// THIS PORTION IS INCOMPLETE SINCE THE SIMULATOR DOESN'T WORK FOR EMAILS
+// I PURPOSELY DID NOT IMPLEMENT ATTACHING THE IMAGE FROM THE SOURCE, BECAUSE OF IT.
+// AS WELL AS GETTING THE EMAIL BODY TEXT AND SPLASH SCREEN IF SUCCESSFULLY SENT
+- (IBAction)displayComposerSheet:(id)sender{
+    
+    MFMailComposeViewController *mailComposer = [[MFMailComposeViewController alloc] init];
+    mailComposer.mailComposeDelegate = self;
+    
+    // Set the title
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *loadTitleText = [defaults objectForKey:@"titleText"];
+    NSString *organizationTitle = @"AggieFix_";
+    NSString *wholeTitle = [organizationTitle stringByAppendingString:loadTitleText];
+    [mailComposer setSubject:wholeTitle];
+    
+    // Set up the recipients. // pass NSArray
+    [mailComposer setToRecipients:@[@"haru.mhl@gmail.com"]];
+    
+    // Attach an image to the email.
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"ipodnano"
+                                                     ofType:@"png"];
+    NSData *myData = [NSData dataWithContentsOfFile:path];
+    [mailComposer addAttachmentData:myData mimeType:@"image/png"
+                     fileName:@"ipodnano"];
+    
+    // Fill out the email body text.
+    NSString *emailBody = @"It is raining in sunny California!";
+    [mailComposer setMessageBody:emailBody isHTML:NO];
+    
+    // Present the mail composition interface.
+    [self presentViewController:mailComposer animated:YES completion:nil];
+        // [self presentModalViewController:mailComposer animated:YES]; since it depreciated
+}
+
+// The mail compose view controller delegate method
+// TO SHOW SPLASH SCREEN, I NEED TO GET THE VALUE OF "RESULT" TO KNOW WHETHER SUCCESSFULLY SENT
+- (void)mailComposeController:(MFMailComposeViewController *)controller
+          didFinishWithResult:(MFMailComposeResult)result
+                        error:(NSError *)error
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+        // [self dismissModalViewControllerAnimated:YES]; since it depreciated
 }
 
 @end
